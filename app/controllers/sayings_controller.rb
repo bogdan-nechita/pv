0c9194727bf7a4918d31d9fbb106fb8064492945
Bogdan Nechita <talk.bogdan@gmail.com>
2014-12-24 00:08:16 +0200

    Add uber saying logic and a to_json method to the Saying model.

--- app/controllers/sayings_controller.rb
+++ app/controllers/sayings_controller.rb
@@ -1,7 +1,21 @@
 class SayingsController < ApplicationController
   def uber_saying
   	# generate two random ids between 0 and the number of the sayings in the database
-  	# read the two sayings with those ids
-  	# return the part1 and separator from the first saying and the part2 from the second saying
+  	random_gen = Random.new(Time.now.to_i)
+  	first_no = random_gen.rand(1..Saying.all.size)
+  	second_no = random_gen.rand(1..Saying.all.size)
+    
+    # read the two sayings with those ids
+  	first_saying = Saying.find(first_no)
+  	second_saying = Saying.find(second_no)
+
+    # return the part1 and separator from the first saying and the part2 from the second saying
+    # if the separator is a comma, don't add an extra space before it
+    part1_padding = first_saying.separator == ',' ? '' : ' '
+  	uber_saying = first_saying.part1 + part1_padding + first_saying.separator + ' ' + second_saying.part2
+  	
+  	#return the uber_saying as well as the original sayings
+  	json_sayings = { uber_saying: uber_saying, first_saying: first_saying.to_json, second_saying: second_saying.to_json}.to_json
+  	render json: json_sayings
   end
 end
--- app/models/saying.rb
+++ app/models/saying.rb
@@ -1,2 +1,5 @@
 class Saying < ActiveRecord::Base
+	def to_json 
+	  { part1: self.part1, separator: self.separator, part2: self.part2 }	
+	end
 end
