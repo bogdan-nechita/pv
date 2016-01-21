f509a694b672a901ffc3a519bb2836501c2e0a0e
Bogdan Nechita <talk.bogdan@gmail.com>
2016-01-21 18:02:25 +0200

    Handle nil separator.

--- app/controllers/sayings_controller.rb
+++ app/controllers/sayings_controller.rb
@@ -8,11 +8,12 @@ class SayingsController < ApplicationController
     # read the two sayings with those ids
   	first_saying = Saying.find(first_no)
   	second_saying = Saying.find(second_no)
+    separator = first_saying.separator.nil? ? ' ' : first_saying.separator
 
     # return the part1 and separator from the first saying and the part2 from the second saying
     # if the separator is a comma, don't add an extra space before it
-    part1_padding = first_saying.separator == ',' ? '' : ' '
-  	dada_saying = first_saying.part1 + part1_padding + first_saying.separator + ' ' + second_saying.part2
+    part1_padding = separator == ',' ? '' : ' '
+    dada_saying = first_saying.part1 + part1_padding + separator + ' ' + second_saying.part2
   	
   	#return the dada_saying as well as the original sayings
   	json_sayings = { dada_saying: dada_saying, first_saying: first_saying.to_json, second_saying: second_saying.to_json}.to_json
